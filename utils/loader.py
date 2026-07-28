a23684597ff4c67e8a749e5139381e2e6514070d
coldcode_ <118668883+Frankiee77@users.noreply.github.com>
2026-07-28 15:59:22 +0700

    Create loader.py

---
+++ utils/loader.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import pickle
+import streamlit as st
+from tensorflow.keras.models import load_model
+
+MODEL_DIR = Path("models")
+
+# ---------------- LSTM ----------------
+
+@st.cache_resource
+def load_lstm():
+
+    model = load_model(
+        MODEL_DIR / "lstm_dbd.keras",
+        compile=False
+    )
+
+    return model
+
+
+@st.cache_resource
+def load_lstm_scaler():
+
+    with open(MODEL_DIR / "scaler_target.pkl", "rb") as f:
+        scaler_target = pickle.load(f)
+
+    with open(MODEL_DIR / "scaler_features.pkl", "rb") as f:
+        scaler_feature = pickle.load(f)
+
+    return scaler_target, scaler_feature
+
+
+# ---------------- SARIMAX ----------------
+
+@st.cache_resource
+def load_sarimax():
+
+    with open(MODEL_DIR / "sarimax_artifacts.pkl", "rb") as f:
+        model = pickle.load(f)
+
+    return model
